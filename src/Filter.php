8b96bf462feabba5826c5c0f9c9712e98aa25906
maxgu <gulturyan@t4web.com.ua>
2015-11-11 17:48:21 +0200

    draft Filter

---
+++ src/Filter.php
@@ -0,0 +1,14 @@
+<?php
+namespace T4webFilter;
+
+class Filter implements FilterInterface
+{
+    /**
+     * @param array $inputData
+     * @return array
+     */
+    public function prepare(array $inputData = [])
+    {
+        return [];
+    }
+}
