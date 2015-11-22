729f40993b1d898b12e905016b50c0cb0e9cae40
maxgu <maxgu@t4web.com.ua>
2015-11-22 15:17:03 +0200

    add defaults

--- src/Filter.php
+++ src/Filter.php
@@ -3,12 +3,40 @@ namespace T4webFilter;
 
 class Filter implements FilterInterface
 {
+    protected $defaults;
+
     /**
-     * @param array $inputData
+     * @param array $defaults
+     */
+    public function __construct(array $defaults = [])
+    {
+        $this->defaults = $defaults;
+    }
+
+    /**
+     * @param array $filter
      * @return array
      */
-    public function prepare(array $inputData = [])
+    public function prepare(array $filter = [])
     {
-        return [];
+        if (empty($filter['limit'])) {
+            $filter['limit'] = $this->getDefault('limit', 20);
+        }
+        if (empty($filter['offset'])) {
+            $filter['offset'] = $this->getDefault('offset', 0);
+        }
+
+        return $filter;
+    }
+
+    /**
+     * @param      $attribute
+     * @param mixed $valueIfNotExists
+     *
+     * @return mixed
+     */
+    private function getDefault($attribute, $valueIfNotExists = null)
+    {
+        return isset($this->defaults[$attribute]) ? $this->defaults[$attribute] : $valueIfNotExists;
     }
 }
